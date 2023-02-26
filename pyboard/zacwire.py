eca0f4ebcb50c687227ffd3e1a38746bd39dd196
mdaeron <mathieu@daeron.fr>
2023-02-26 17:41:24 +0100

    Add parity check

--- pyboard/zacwire.py
+++ pyboard/zacwire.py
@@ -1,9 +1,14 @@
 from pyb import ExtInt, Pin, micros, Timer
 from utime import ticks_diff
 from array import array
+from micropython import schedule
 
 
 class ZACwire():
+
+	# values chosen to yield apparent temperatures below absolute zero:
+	_WRONG_PARITY   = -292107 # yields apparent temperature around -9999
+	_NO_READING_YET = -97174  # yields apparent temperature around -3333
 
 	def __init__(self, pin, timer = 1):
 		self.timer = Timer(timer)
@@ -13,10 +18,11 @@ class ZACwire():
 		self.dt = array('l', [0]*(self.buflen-1))
 		self._cb_irq = self.cb_irq
 		self._cb_timer = self.cb_timer
+		self._decode = self.decode
 		self.ei = ExtInt(pin, ExtInt.IRQ_RISING_FALLING, Pin.PULL_NONE, self._cb_irq)
-		self.bitslen = 13
+		self.bitslen = 14
 		self.bits = array('b', [0]*self.bitslen)
-		self.rawT = 0
+		self.rawT = ZACwire._NO_READING_YET
 
 	def cb_irq(self, _):
 		# this takes ~35 us if self.bufloc = 0, ~15 us otherwise
@@ -29,27 +35,37 @@ class ZACwire():
 		# this takes ~1 ms
 		self.timer.deinit()
 		self.bufloc = 0
-		self.decode()
+		schedule(self._decode, None)
 
-	def decode(self):
+	def decode(self, _):
 		for k in range(self.buflen-1):
 			self.dt[k] = ticks_diff(self.buf[k+1], self.buf[k])
 
 		threshold = self.dt[0]
 
-		for j in range(9):
+		for j in range(self.bitslen):
 			self.bits[-1-j] = self.dt[-(j+1)*2] < threshold
-		for j in range(4):
-			self.bits[-10-j] = self.dt[-(j+11)*2] < threshold
 
-		# TODO: ADD PARITY CHECK
-		
-		self.rawT = self.bits[11]
+		parity = self.bits[-2]
 		for k in range(7):
-			self.rawT += self.bits[10-k] << k+1
+			parity += self.bits[-3-k]
+		if (parity % 2) != self.bits[-1]:
+			self.rawT = ZACwire._WRONG_PARITY
+			return None
+
+		parity = self.bits[-12]
+		for k in range(2):
+			parity += self.bits[-13-k]
+		if (parity % 2) != self.bits[-11]:
+			self.rawT = ZACwire._WRONG_PARITY
+			return None
+	
+		self.rawT = self.bits[-2]
+		for k in range(7):
+			self.rawT += self.bits[-3-k] << k+1
 		for k in range(3):
-			self.rawT += self.bits[2-k] << k+8
-		
+			self.rawT += self.bits[-12-k] << k+8
+
 	def T(self):
 		return self.rawT / 2047 * 70 - 10
 
